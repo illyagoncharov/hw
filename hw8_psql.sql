469117947b88839ce0b128ed852f22060bae6b6e
illyagoncharov <146721047+illyagoncharov@users.noreply.github.com>
2023-11-23 10:09:21 +0200

    Add files via upload
    many to many films actors

--- hw8_psql.sql
+++ hw8_psql.sql
@@ -1,46 +1,52 @@
-Create table actors(id serial, first_name varchar(30), 
-				last_name varchar(30), date_of_birth date, 
-				gender varchar(30));
-					
-Create table directors(id serial, first_name varchar(30), 
-				last_name varchar(30), email varchar(50));
-					   
-Create table films(id serial, film_name varchar(30),
-				 ganre varchar(30), country varchar(30), 
-				 id_director integer, id_actor integer) ;
+Create table country(id serial primary key, name varchar(50));
 
-INSERT INTO actors (first_name, last_name, date_of_birth, gender) 
-VALUES ('John', 'Travolta', 'Feb 18 1954', 'MALE');
-INSERT INTO actors (first_name, last_name, date_of_birth, gender) 
-VALUES ('Willard', 'Smith', 'Sep 25 1968', 'MALE');
-INSERT INTO actors (first_name, last_name, date_of_birth, gender) 
-VALUES ('Leonardo', 'DiCaprio', 'Nov 11 1974', 'MALE');
+Insert into country (name) 
+values ('Argentina'),('UK'), ('France'),('China'),('USA'),('Italia'),('Japan'),('Australia'),('Ukraine');
 
-INSERT INTO directors (first_name, last_name, email) 
-VALUES ('Quentin', 'Tarantino', 'tarantino@gmail.com');
-INSERT INTO directors (first_name, last_name, email) 
-VALUES ('Martin', 'Scorsese', 'scorsese@gmail.com');
-INSERT INTO directors (first_name, last_name, email) 
-VALUES ('Guy', 'Ritchie', 'guy@gmail.com');	
+CREATE TABLE gender ( id serial primary key, name varchar(50));
 
-INSERT INTO films(film_name, ganre, country, id_director, id_actor)
-VALUES ('Aladdin', 'Adventures', 'Australia, USA, UK', '3', '3');
-INSERT INTO films(film_name, ganre, country, id_director, id_actor)
-VALUES ('Pulp Fiction', 'Crime novels', 'USA', '1', '1');
-INSERT INTO films(film_name, ganre, country, id_director, id_actor)
-VALUES ('The Wolf of Wall Street', 'Biographical ', 'USA', '2', '2');
+Insert into gender(name)
+Values ('male'), ('female');
 
-select pp.film_name, concat (p.first_name, ' ', p.last_name) as director, 
-		concat (cp.first_name, ' ', cp.last_name) as actor 
-		from films pp
-	left join people p on pp.director = p.id 
-	left join people cp on pp.main_actor = cp.id;
+create table profession ( id serial primary key, name varchar(20));
 
-Select film_name, genre_name as genre, country_name as country, concat(pl.first_name, ' ', pl.last_name) as director,
+Insert into profession ( name)
+Values ('actor'), ('director');
+
+Create table genre ( id serial primary key, name varchar(50));
+
+Insert into genre (name)
+Values ('Adventure'), ('Comedy'), ('Drama'), ('Fantasy'), ('Historical'), 
+('Horror'), ('Thriller'), ('Western'), ('Biographical');
+
+Create table people( id serial primary key, first_name varchar(50), 
+					last_name varchar(50), email varchar(50), 
+					id_gender integer references gender(id), 
+					id_profession integer references profession(id));
+
+Insert into people ( first_name, last_name, email, id_gender, id_profession)
+
+Values ('John', 'Travolta', 'travolta@gmail.com', 1, 1 ), 
+('Willn', 'Smith', 'ws@gmail.com', 1, 1 ),
+('Leonardo', 'DiCaprio', 'ld@gmail.com', 1, 1 ),
+('Quentin', 'Tarantino', 'tarantino@gmail.com', 1, 2 ),
+('Martin', 'Scorsese', 'ms@gmail.com', 1, 2 ),
+('Guy', 'Ritchie', 'guy@gmail.com', 1, 2 );
+
+Create table films(id serial primary key, name varchar(50),
+				   id_genre integer references genre(id),id_country integer references country(id), 
+				   id_director integer references people(id), id_main_actor integer references people(id) );
+
+Insert into films(name, id_genre, id_country, 
+				   id_director, id_main_actor)
+
+Values ('Aladdin', 1, 8, 6, 2), ('Pulp Fiction', 7, 5, 4, 1), ('The Wolf of Wall Street', 9, 5, 5, 3);
+
+
+Select f.name, genre.name as genre, country.name as country, concat(pl.first_name, ' ', pl.last_name) as director,
 concat(people.first_name, ' ', people.last_name) as actor
-
 from films f
 	left join genre on genre.id = f.id_genre
 	left join country on country.id = f.id_country
-	left join people pl on pl.id = f.director
-	left join people on people.id = f.main_actor; 
+	left join people pl on pl.id = f.id_director
+	left join people on people.id = f.id_main_actor; 
