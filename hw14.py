5b64d13c1cd1e5a78f46811559ccc52e8271b942
illyagoncharov <146721047+illyagoncharov@users.noreply.github.com>
2023-12-13 18:44:32 +0200

    Update hw14.py

--- hw14.py
+++ hw14.py
@@ -1,5 +1,7 @@
 import requests
 import csv
+from constants import Indexmodule
+from urllib.parse import urlparse
 
 class Candidate:
     list_candidates = []
@@ -25,46 +27,50 @@ class Candidate:
         data_with_candidates = cls.create_candidates_list(read_file)
         res_candidates_list = []
         for i in data_with_candidates:
-            can = cls(*i)
+            can = cls(**i)
             res_candidates_list.append(can)
         return res_candidates_list
 
     @classmethod
     def read_the_file(cls, file_name):
-        try:
+        url_parsed = urlparse(file_name)
+        if url_parsed.scheme in ('http', 'https'):
+            cls.open_url(file_name)
+            read_file = cls.open_file(Constans._file_name)
+        else:
             read_file = cls.open_file(file_name)
-        except OSError:
-            #print('try open url')
-            local_file_name = cls.open_url(file_name)
-            read_file = cls.open_file(local_file_name)
         return read_file
 
     @staticmethod
     def open_file(file_name):
-        with open('candidates.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-            data_with_candidates = []
-            for i in reader:
-                data_with_candidates.append(i)
-        return data_with_candidates
+        with open(file_name, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+            list_candidates = []
+            for pow in reader:
+                list_candidates.append(pow)
+        return list_candidates
 
     @staticmethod
     def open_url(url_adress):
         response = requests.get(url_adress)
-        file_name = "urlcandidates.csv"
-        open(file_name, "wb").write(response.content)
-        return file_name
+        with open(Constans._file_name,  'wb') as csvfile:
+            csvfile.write(response.content)
+        return
 
     @staticmethod
     def create_candidates_list(data_with_candidates):
-        for i in data_with_candidates:
-            j = i[0].split()
-            i.remove(i[0])
-            i.insert(0, j[1])
-            i.insert(0, j[0])
-        data_with_candidates.reverse()
-        data_with_candidates.pop()
-        return data_with_candidates
+        candidates = []
+        candidate = {}
+        for pow in data_with_candidates:
+            candidate['first_name'] = pow['Full Name'].split()[Constans.FIRST_NAME_INDEX]
+            candidate['last_name'] = pow['Full Name'].split()[Constans.LAST_NAME_INDEX]
+            candidate['email'] = pow['Email']
+            candidate['tech_stack'] = pow['Technologies']
+            candidate['main_skill'] = pow['Main Skill']
+            candidate['main_skill_grade'] = pow['Main Skill Grade']
+            candidates.append(candidate)
+            candidate = candidate.copy()
+        return candidates
 
 
 adress = 'candidates.csv'
